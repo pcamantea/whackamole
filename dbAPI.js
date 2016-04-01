4c2fc8421982f5ee93e7ddc127dbace7951c8c37
take-iwiw <take.iwiw2222@gmail.com>
2016-04-01 18:40:46 -0400

    Update dbAPI.js

--- dbAPI.js
+++ dbAPI.js
@@ -3,6 +3,7 @@ var postScore = function(name, score){
 }
 
 var getRanking = function(){
+	// the order must be [1st ranker, 2nd ranker, 3rd ranker]
 	var ranking =
 		[ {"name":"ABC", "score":999}, {"name":"DEF", "score":555}, {"name":"GHI", "score":111} ];
 	return ranking;
