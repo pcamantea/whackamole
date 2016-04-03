cd3778ec3e429c358a5a35cee857f31d02c78fa3
pcamantea <pca1987+github@gmail.com>
2016-04-03 18:13:16 -0400

    adding 50 tries option and gameover

--- whackamole.js
+++ whackamole.js
@@ -5,18 +5,12 @@ var phonegapReady = function(){
 	var viewportScale = 1 / window.devicePixelRatio;
 	startGameScope();
 	setRanking();
-
+	
+	// add misclick handler on all holes
+	$(".hole").on("click", missClickHandler);
+	
 	// todo: delete this. this is just sample code
-	$("#giveup").click(function() {
-		// maybe need to stop all timers.
-		gPoint = Number($("#points").text());
-		setRanking();
-		window.location.href = '#pageone';
-		
-		GAME_ON = false;
-		clearTimeout(currentTimeout);
-		resetGameValues();
-	});
+	$("#giveup").click(gameOver);
 }
  
 document.addEventListener("deviceready", phonegapReady, false);
@@ -25,10 +19,22 @@ document.addEventListener("touchmove", function(event) { event.preventDefault();
 $( document ).ready(function() {
 });
 
+gameOver = function() {
+	// maybe need to stop all timers.
+	gPoint = Number($("#points").text());
+	setRanking();
+	window.location.href = '#pageone';
+	
+	GAME_ON = false;
+	clearTimeout(currentTimeout);
+	resetGameValues();
+}
+
 resetGameValues = function() {
-	$(".hole").removeClass("mole").off("click");
+	$(".hole").removeClass("mole").off("click").on("click", missClickHandler);
 	$("#points").text('0');
 	$("#level").text('1');
+	$("#tries").text('50');
 }
 
 startGameScope = function() {
@@ -55,7 +61,7 @@ startGameScope = function() {
 			if ($previousHole != null)
 			{
 				// Turn off previous hole and remove tapHandler
-				$previousHole.removeClass("mole").off("click", tapHandler);
+				$previousHole.removeClass("mole").off("click").on("click", missClickHandler);
 			}
 			
 			tapHandler = function () {
@@ -84,14 +90,29 @@ startGameScope = function() {
 				}
 				
 				// change hole style to smashed and remove tap handler
-				$nextHole.addClass("deadMole").removeClass("mole").off("click", tapHandler); 
+				$nextHole.addClass("deadMole").removeClass("mole").off("click", tapHandler).on("click", missClickHandler);
 				
 				// remove clicked hole style after 250ms
 				setTimeout(function() { $nextHole.removeClass("deadMole") }, 250);
+				
+			}
+			
+			missClickHandler = function () {
+				// decrement tries
+				var currentTries = $("#tries").text();
+				
+				if (currentTries == 1) {
+					// game over
+					gameOver();
+					return;
+				}
+				
+				var newTries = Number(currentTries) - 1;
+				$("#tries").text(newTries);
 			}
 
 			// hold hole reference for the next round
-			$previousHole = $nextHole.addClass("mole").on("click", tapHandler);
+			$previousHole = $nextHole.addClass("mole").off("click").on("click", tapHandler);
 		  
 			// wait for 1 second and play next round
 			currentTimeout = setTimeout(playNextRound, $time);
